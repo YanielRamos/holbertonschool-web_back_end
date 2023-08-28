7c597956380cd77a8fd83754aee21d7d7c718ba8
YanielRamos <yanielramos0504@gmail.com>
2023-08-28 13:47:52 -0400

    updating

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,7 +2,7 @@
 """module that write a type-annotated function add that takes a float a
 and a float b as arguments and returns their sum as a float"""
 
-if __name__ == '__main__':
-    def add(a: float, b: float) -> float:
-        """function that sum two floatsa nd return a float"""
-        return a + b
+
+def add(a: float, b: float) -> float:
+    """function that sum two floatsa nd return a float"""
+    return a + b

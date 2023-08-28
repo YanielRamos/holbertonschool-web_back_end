bc8c89a5aa43179a221a6f8cdc7babae7e4a17b0
YanielRamos <yanielramos0504@gmail.com>
2023-08-28 15:36:46 -0400

    adding file 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""script that have function with a list of float as argument"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+
+    result = sum(input_list)
+
+    return result

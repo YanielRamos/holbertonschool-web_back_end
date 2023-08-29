b4e52d9ec05ede99a4d6c936dc68a2eca97e9142
YanielRamos <yanielramos0504@gmail.com>
2023-08-29 12:01:23 -0400

    updating file

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,5 +7,6 @@ import random
 @asyncio.coroutine
 async def wait_random(max_delay=10):
     """selectnumber a random """
-    await asyncio.sleep(random.randint(0, max_delay))
-    return float(max_delay)
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay

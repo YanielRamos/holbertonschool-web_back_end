dd53bd92d8786872ea503de9d8136824b1acf2f5
YanielRamos <yanielramos0504@gmail.com>
2023-08-29 15:46:32 -0400

    fixing requirements

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Script that return a asyncio task"""
 import asyncio
+from typing import Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> Any:
     """function"""
     cls_type = asyncio.Task(wait_random(max_delay))
     return cls_type

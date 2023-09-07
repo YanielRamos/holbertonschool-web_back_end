b9c0566c67ae147f80a97f4d7bf16a8c6a7ade5b
Yaniel Ramos <yanielramos0504@gmail.com>
2023-09-06 23:26:28 -0400

    adding file 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that inserts a new doc"""
+
+def insert_school(mongo_collection, **kwargs):
+    """function that adds the doc"""
+
+    new_school = mongo_collection.insert_one(kwargs)
+
+    return new_school.inserted_id
+    

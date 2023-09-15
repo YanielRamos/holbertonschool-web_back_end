256b8a0dfa6141febdf794396ff6662b67d6e163
Yaniel Ramos <yanielramos0504@gmail.com>
2023-09-15 12:32:43 -0400

    fixing 6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,20 +1,28 @@
+// Extend Buildinf
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+/**
+ * Define Class
+ *
+ * @class SkyHighBuilding
+ */
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
-
+  
   get sqft() {
     return this._sqft;
   }
 
   get floors() {
-    return this._floors;
+    return (this._floors);
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+    return (`Evacuate slowly the ${this.floors} floors`);
   }
 }
+
+export default SkyHighBuilding;

6f65dc371e25ae115a869893603300333fa330c5
YanielRamos <yanielramos0504@gmail.com>
2023-10-09 19:43:34 -0400

    fixing directory

--- ES6_data_manipultaion/3-get_ids_sum.js
+++
@@ -1,9 +0,0 @@
-/* enlist-disable */
-export default function getStudentIdsSum(students) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-  
-    const reduce = students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
-    return reduce;
-  }
